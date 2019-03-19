3f430210be32c47ca3c0824c9a801c28e5954373
Jie Shen <drfish.me@gmail.com>
2019-03-19 14:48:26 +0800

    Change some configuration display names

--- src/main/java/com/microsoft/jenkins/artifactmanager/Constants.java
+++ src/main/java/com/microsoft/jenkins/artifactmanager/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public static final String ARTIFACTS_PATH = "artifacts/";
     public static final String STASHES_PATH = "stashes/";
     public static final String TGZ_FILE_EXTENSION = ".tgz";
-    public static final String AZURE_STORAGE_DISPLAY_NAME = "Azure Storage";
+    public static final String AZURE_STORAGE_DISPLAY_NAME = "Azure Blob Storage";
     public static final String BUILD_PREFIX_FORMAT = "%s/%s";
     public static final String VIRTUAL_PATH_FORMAT = "%s%s/%s";
     public static final String COMMA = ",";
