154095c109a8084f3b48aea213a278815843d74a
Tim Jacomb <timjacomb1+github@gmail.com>
2021-05-06 15:06:38 +0100

    Unused imports

--- src/test/java/com/microsoft/jenkins/artifactmanager/IntegrationTests/ArtifactManagerIT.java
+++ src/test/java/com/microsoft/jenkins/artifactmanager/IntegrationTests/ArtifactManagerIT.java
@@ -24,7 +24,6 @@ import org.apache.commons.io.FileUtils;
 import org.jenkinsci.plugins.workflow.ArtifactManagerTest;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
