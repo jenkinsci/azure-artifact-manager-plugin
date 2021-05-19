4d3e3d0d04827849ef69c9c472f560c1a15ba246
Tim Jacomb <timjacomb1+github@gmail.com>
2021-05-19 08:21:53 +0100

    Fix creating new container

--- src/main/java/com/microsoft/jenkins/artifactmanager/AzureArtifactManager.java
+++ src/main/java/com/microsoft/jenkins/artifactmanager/AzureArtifactManager.java
@@ -125,7 +125,7 @@ public final class AzureArtifactManager extends ArtifactManager implements Stash
                 .act(new ContentTypeGuesser(new ArrayList<>(artifacts.keySet()), listener));
 
         try {
-            BlobContainerClient container = Utils.getBlobContainerReference(accountInfo, config.getContainer(), false);
+            BlobContainerClient container = Utils.getBlobContainerReference(accountInfo, config.getContainer(), true);
 
             for (Map.Entry<String, String> entry : contentTypes.entrySet()) {
                 String path = "artifacts/" + entry.getKey();
